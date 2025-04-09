120e8918dc0ff04008312a5f733ff2b5642ee181
Marios Philippou <mariosphilippou145@gmail.com>
2025-04-09 11:33:37 +0300

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/templates/**/*.{html,js}",
+    "./src/static/js/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
